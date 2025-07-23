7c75abb30a7ec09115e2b6d0938c127b4d814817
Gabriel Rocha Dias <gabigolbr26@gmail.com>
2025-07-22 22:06:00 -0300

    add files

--- routes/empresas.py
+++ routes/empresas.py
@@ -4,7 +4,8 @@ empresas_bp = Blueprint('empresas', __name__)
 
 @empresas_bp.route("/empresas", methods=["GET"])
 def return_page():
-        return render_template("index.html", aba="empresas")
+        empresas = [{"id": 1,"nome": "TechMaster LTDA","cnpj": "12.345.678/0001-90"}]
+        return render_template("index.html", aba="empresas", empresas=empresas)
 
 
 @empresas_bp.route("/empresas/empresa", methods=["POST"])
