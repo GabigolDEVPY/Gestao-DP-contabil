affe0be9fc97a640b1e3508049da884a172c1c45
Gabriel rocha dias <gabigolbr36@gmail.com>
2025-08-29 20:53:31 -0300

    add files

--- routes/contas.py
+++ routes/contas.py
@@ -5,10 +5,5 @@ contas_bp = Blueprint('contas', __name__)
 
 @contas_bp.route("/contas", methods=["GET"])
 def return_page():
-    contas_data = [
-        {"descricao": "INSS", "valor": 500.00, "data_vencimento": "2025-07-25"},
-        {"descricao": "FGTS", "valor": 300.00, "data_vencimento": "2025-07-30"},
-    ]
     contas = Contas.retornar_contas()
-    print(contas)
     return render_template("contas.html", aba="contas", contas=contas)
