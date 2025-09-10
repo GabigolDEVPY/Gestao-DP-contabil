276aee42b3b8ca8637c56596c6c97e339b14a70c
Gabriel Rocha Dias <gabigolbr26@gmail.com>
2025-09-09 21:47:56 -0300

    add files

--- routes/contas.py
+++ routes/contas.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from utils.func_contas import Contas
 
 contas_bp = Blueprint('contas', __name__)
@@ -6,5 +6,11 @@ contas_bp = Blueprint('contas', __name__)
 @contas_bp.route("/contas", methods=["GET"])
 def return_page():
     contas = Contas.retornar_contas()
-    return render_template("contas.html", aba="contas", contas=contas)
+    return render_template("contas.html", contas=contas)
 
+
+@contas_bp.route("/contas/criar", methods=["POST"])
+def criar_conta():
+    dados = request.form.to_dict()
+    print(dados)
+    redirect(url_for("contas.return_page"))
