a58582a62f74ecbe340aaff1ca74c85f0912cf4e
Gabriel rocha dias <gabigolbr36@gmail.com>
2025-09-17 21:04:41 -0300

    add files

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,26 @@
 import sys
 sys.dont_write_bytecode = True
+
 from waitress import serve
 from app import app
+import logging
+from werkzeug.middleware.proxy_fix import ProxyFix
+from flask import request
+from banner import printar
+@app.before_request
+def log_request_info():
+    app.logger.info(f"Request: {request.method} {request.url}")
+    
+@app.after_request
+def log_response_info(response):
+        app.logger.info(f"{request.method} {request.url} -> {response.status_code}")
+        return response
+    
 
 if __name__ == "__main__":
-    print("Server ativo em 5600")
+    port = 5600
+    logging.basicConfig(level=logging.INFO)
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+    printar()
+    print(f"Server rodando na porta 5600")
     serve(app, host="0.0.0.0", port=5600)
